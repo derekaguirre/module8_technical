8206105bfdca5a97928f71e866dfe09e154984ee
bamariscal <bamariscal@miners.utep.edu>
2022-04-15 17:16:20 -0600

    Manage Packets deleted

--- ManagePackets.py
+++
@@ -1,9 +0,0 @@
-# Team 7
-# Ctrl + Alt + Elite
-print("Hello world")
-print("Goodbye world")
-print("Anyone know how to get winning lottery ticket without payin too much?")
-print("Please i need the money")
-print("We can try hacking the powerball right? we got a team of 8 cs majors")
-print("Down")
-print("it worked")
