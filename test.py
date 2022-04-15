dba9b9c96c48da8dd2e1994bc05284bd77a70795
lmartinez50 <60719822+lmartinez50@users.noreply.github.com>
2022-04-15 05:01:49 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import can_decoder
 import mdf_iter
 
-mdf_path = "00002083.MF4"
+mdf_path = "00002083.MF4"   # It needs to be read from the "Open" option in Visualizer.py so we can select any file
 dbc_path = "SAE-J1939-Truck.dbc"
 
 db = can_decoder.load_dbc(dbc_path)
@@ -11,5 +11,9 @@ with open(mdf_path, "rb") as handle:
     mdf_file = mdf_iter.MdfFile(handle)
     df_raw = mdf_file.get_data_frame()
 
+
 df_phys = df_decoder.decode_frame(df_raw)
-print(df_phys)
+
+
+def function1():
+    print(df_phys)
