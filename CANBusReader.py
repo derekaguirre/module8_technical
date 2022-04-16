3d539df3e502aba641bdf3fee464f1d28d4c9c34
Efrain Retana <eretana1@miners.utep.edu>
2022-04-16 17:38:21 -0400

    Create CAN bus reader class.
    Included can dbc file for testing purposes, and updated README.
    CAN bus reader will be used with UI

--- CANBusReader.py
+++ CANBusReader.py
@@ -1,15 +1,34 @@
 import os
-import threading
 import subprocess
-import sys
+import signal
 
 
-# Command to read packets from CAN bus
-subprocess.Popen(['candump -u -L vcan0 > CapturedPackets.pcap'], shell=True) # Create background process to run shell command
+# Read packets from CAN Bus and save them into Captured and Decoded files
+class CANBusReader:
+    def __init__(self, dbc_file):
+        self.dbc_file = dbc_file
 
-# Exit command
-user_input = input('Type STOP to end live capture: ')
-if user_input.upper() == 'STOP':
-    sys.exit(1)
+        # Init process variables
+        self.can_sniff = None
+        self.can_gen = None
+        self.can_decode = None
 
+
+    # Command to read packets Live from CAN bus
+    # Used when user toggles/enables live feed.
+    def start_live_feed(self):
+        # Generate/open file to store packets
+        cp_file = open('CapturedPackets.pcap', 'w') 
+        dec_file = open('DecodedPackets.pcap', 'w')
+        self.can_sniff = subprocess.Popen(['cansniffer vcan0'], shell=True, stdout=cp_file, preexec_fn=os.setsid) # Create sniffing process
+        self.can_gen = subprocess.Popen(['cangen vcan0'], shell=True, preexec_fn=os.setsid) # Create CAN generating process
+        self.can_decode = subprocess.Popen([f'candump vcan0 | python3 -m cantools decode {self.dbc_file}'], shell=True, stdout=dec_file, preexec_fn=os.setsid) # Create background decoding process
+
+
+    # Kill existing running processes used to fetch and decode live feed
+    # This method is only callend after start_live_feed has been called
+    def stop_live_feed(self):
+        os.killpg(os.getpgid(self.can_sniff.pid), signal.SIGKILL)  # Send signal to processes currently running
+        os.killpg(os.getpgid(self.can_gen.pid), signal.SIGTERM)  
+        os.killpg(os.getpgid(self.can_decode.pid), signal.SIGTERM)  
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # module8_technical
 This repo serves as a demonstration for a portion of functionality relating to the CAN Bus Visualizer tool currently in the design process.
+
+* Note: `honda.dbc` is used as a test dbc file to decode the contents of the virtual can bus.
