170b8c1fe81eac9446a805cf54c94333c346c9f9
Efrain Retana <eretana1@miners.utep.edu>
2022-04-15 20:35:16 -0400

    Add flag to update file withouth buffer(real time)

--- CANBusReader.py
+++ CANBusReader.py
@@ -5,9 +5,11 @@ import sys
 
 
 # Command to read packets from CAN bus
-subprocess.Popen(['candump -L vcan0 > CapturedPackets.pcap'], shell=True) # Create background process to run shell command
+subprocess.Popen(['candump -u -L vcan0 > CapturedPackets.pcap'], shell=True) # Create background process to run shell command
+
+# Exit command
 user_input = input('Type STOP to end live capture: ')
-if user_input.upper() == 'stop':
+if user_input.upper() == 'STOP':
     sys.exit(1)
 
 
