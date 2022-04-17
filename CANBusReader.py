886a5dce62be1e98838fc7745a14b88e04dd4c96
Efrain Retana <eretana1@miners.utep.edu>
2022-04-17 13:07:36 -0400

    Update CANBus reader to decode known packets.
    Update ReadMe to include usage steps.

--- CANBusReader.py
+++ CANBusReader.py
@@ -22,7 +22,7 @@ class CANBusReader:
         dec_file = open('DecodedPackets.pcap', 'w')
         self.can_sniff = subprocess.Popen(['cansniffer vcan0'], shell=True, stdout=cp_file, preexec_fn=os.setsid) # Create sniffing process
         self.can_gen = subprocess.Popen(['cangen vcan0'], shell=True, preexec_fn=os.setsid) # Create CAN generating process
-        self.can_decode = subprocess.Popen([f'candump vcan0 | python3 -m cantools decode {self.dbc_file}'], shell=True, stdout=dec_file, preexec_fn=os.setsid) # Create background decoding process
+        self.can_decode = subprocess.Popen([f'candump vcan0,0x255:0x7FF,0x201:0x7FF,0x35E:0x7FF,0x296:0x7FF,0x158:0x7FF,0x13C:7FF,0x1AB:0x7FF,0x1D0:0x7FF,0x1A3:0x7FF,0x221:0x7FF,0x300:0x7FF,0x516:0x7FF,0x200:0x7FF,0x221:0x7FF,0x184:0x7FF,0x1C2:7FF,0x14A:0x7FF | python3 -m cantools decode {self.dbc_file}'], shell=True, stdout=dec_file, preexec_fn=os.setsid) # Create background decoding process
 
 
     # Kill existing running processes used to fetch and decode live feed
@@ -31,4 +31,3 @@ class CANBusReader:
         os.killpg(os.getpgid(self.can_sniff.pid), signal.SIGKILL)  # Send signal to processes currently running
         os.killpg(os.getpgid(self.can_gen.pid), signal.SIGTERM)  
         os.killpg(os.getpgid(self.can_decode.pid), signal.SIGTERM)  
-
--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 This repo serves as a demonstration for a portion of functionality relating to the CAN Bus Visualizer tool currently in the design process.
 
 * Note: `honda.dbc` is used as a test dbc file to decode the contents of the virtual can bus.
+
+## Required set-Up
+In order to utilize this tool, you need to be running on a linux-based system.
+
+### Step 1
+`sudo apt-get install can-utils`
+### Step 2
+`pip install cantools`
